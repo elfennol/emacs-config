d7dd6cb7651d378b10ca45680c92acb85163bea2
Elfennol <110494436+elfennol@users.noreply.github.com>
2025-03-23 16:54:19 +0100

    fix(langtool): add totalEntitySizeLimit java option

--- my/conf/langtool.el
+++ my/conf/langtool.el
@@ -2,6 +2,6 @@
 ;; https://github.com/mhayashi1120/Emacs-langtool
 (setq langtool-java-classpath
       "/usr/share/languagetool:/usr/share/java/languagetool/*")
-(require 'langtool)
+(setq langtool-java-user-arguments '("-Djdk.xml.totalEntitySizeLimit=800000"))
 ;; https://github.com/mhayashi1120/Emacs-langtool
 (setq langtool-default-language "en-US")
